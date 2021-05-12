cdcc7fefdfdfd473ca31a15755efba1f260e4a86
quatryle johnson <quatryle.johnson@revature.net>
2021-05-12 09:12:20 -0400

    yaml file updated attempt 5

--- PizzaBox.Testing/Tests/PizzaTests.cs
+++ PizzaBox.Testing/Tests/PizzaTests.cs
@@ -1,11 +1,15 @@
+using PizzaBox.Domain.Models;
+using PizzaBox.Storage;
+using Xunit;
+
 namespace PizzaBox.Testing.Tests
 {
   public class PizzaTests
   {
     public void Test_PizzaConstructor()
-    {/*
+    {
       var sut = new Pizza();
-      Assert.NotNull(sut);*/
+      Assert.NotNull(sut);
     }
 
   }
